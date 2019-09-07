4a234d0d6060bb35ee99c6d601f126b08942844e
HazimSamara <54385811+HazimSamara@users.noreply.github.com>
2019-09-07 00:15:14 -0500

    Update Day03_Variables.java

--- src/day03_Variables/Day03_Variables.java
+++ src/day03_Variables/Day03_Variables.java
@@ -3,9 +3,9 @@ package day03_Variables;
 public class Day03_Variables {
 	
 	public static void main(String[] args) {
-		
+		// update
 		System.out.println("git first day");
-		System.out.println("Hello Git");
+		System.out.println("Hello Git!");
 	}
 
 
